c75438b6881fe274cbc4db1f900fcdf3ae846a0c
Karissa McKelvey <krmckelv@gmail.com>
2015-11-29 02:01:24 -0500

    design update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var templater = require('page-router')
 var fs = require('fs')
+var usefulDate = require('useful-date')
+require( 'useful-date/locale/en-GB.js' )
+var relativeDate = require('relative-date')
 var marked = require('marked')
 var Handlebars = require('handlebars')
 
@@ -21,55 +24,63 @@ var routes = [
       })
     },
     onrender: function (params, data) {
-      force(250)
+      force(175)
     }
   },
   {
     url: '/graph',
     template: fs.readFileSync('templates/graph.html').toString(),
     onrender: function () {
-      force(250)
+      force(500)
     }
   },
   {
     url: '/',
     template: fs.readFileSync('templates/home.html').toString(),
-    data: function (params, cb)  {
+    data: function (params, cb) {
       cb({
-        posts: posts.slice(0,4)
+        posts: posts.slice(0, 4)
       })
     },
     onrender: function (params, data) {
-      force(500)
+      force(250)
     }
   },
   {
     url: '/blog',
     template: fs.readFileSync('templates/blog.html').toString(),
-    data: function (params, cb)  {
+    data: function (params, cb) {
       cb({
         posts: posts
       })
     },
     onrender: function (params, data) {
-      force(250)
+      force(175)
     }
   }
 ]
 
 console.log('if not us, who?')
 
-Handlebars.registerHelper('overview', function(passedString) {
+Handlebars.registerHelper('usefulDate', function (date) {
+  return new Date(date).format('n F Y')
+})
+
+Handlebars.registerHelper('relativeDate', function (date) {
+  return relativeDate(new Date(date))
+})
+
+Handlebars.registerHelper('overview', function (passedString) {
   var endIndex = passedString.indexOf('<!-- more -->')
   if (endIndex === -1) endIndex = 300
-  var theString = passedString.substring(0, endIndex);
-  theString += "..."
+  var theString = passedString.substring(0, endIndex)
+  theString += '...'
   return new Handlebars.SafeString(marked(theString))
-});
+})
 
-Handlebars.registerHelper('markdown', function(string) {
-  return new Handlebars.SafeString(marked(string));
-});
+Handlebars.registerHelper('markdown', function (string) {
+  return new Handlebars.SafeString(marked(string))
+})
 
 templater('#content', routes, function (source, data) {
   var template = Handlebars.compile(source)
